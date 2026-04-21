5ecf855d18642d5c8d9b84c65510c7a9192f74ca
Joey McKenzie <joey.mckenzie27@gmail.com>
2026-04-21 16:13:04 -0700

    chore: wip

--- resources/js/pages/cv.tsx
+++ resources/js/pages/cv.tsx
@@ -77,6 +77,7 @@ const skills = [
             '.NET',
             'Azure',
             'Angular',
+            'Vue',
             'Next.js',
             'Java',
             'Spring Boot',
@@ -84,7 +85,7 @@ const skills = [
     },
     {
         category: 'The glue nobody thanks (that also pays my bills)',
-        items: ['AWS', 'Azure', 'Docker', 'Terraform', 'GitHub', 'Kubernetes'],
+        items: ['AWS', 'Docker', 'Terraform', 'GitHub', 'Kubernetes'],
     },
 ];
 
