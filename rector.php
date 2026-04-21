039e59a7cf41b40984a6eee436b3e9201396947b
Joey McKenzie <joey.mckenzie27@gmail.com>
2026-04-21 13:16:08 -0700

    chore: explicit rector fixes for bootstrap files

--- rector.php
+++ rector.php
@@ -10,15 +10,13 @@ use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
+        __DIR__.'/bootstrap/app.php',
+        __DIR__.'/bootstrap/providers.php',
         __DIR__.'/config',
         __DIR__.'/public',
         __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/tests',
-    ])
-    ->withSkip([
-        __DIR__.'/boostrap/cache',
     ])
     ->withRules([
         AddOverrideAttributeToOverriddenMethodsRector::class,
